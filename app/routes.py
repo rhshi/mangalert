dde97dda9bec26e4e8e12e855fa2d3e917a81d7c
Bobby <bshi1324@gmail.com>
2018-09-16 13:29:08 -0500

    Commit

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from flask import flash, redirect, render_template, url_for
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
 
 from app import app
+from app.models import User
 from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html')
 
@@ -14,7 +18,19 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='sign in', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
