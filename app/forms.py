58fe06f8d7a133956e6b313c4571fa1ad91247ac
Bobby <bshi1324@gmail.com>
2018-09-17 20:07:24 -0500

    commit

--- app/forms.py
+++ app/forms.py
@@ -35,3 +35,13 @@ class EditProfileForm(FlaskForm):
     username = StringFrield('username', validators=[DataRequired()])
     about_me = TextAreaField('about me', validators=Length(min=0, max=140))
     submit = SubmitField('submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('this username is not available')
